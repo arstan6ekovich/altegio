181905d33e9d1913bd8339c50a8b31d50c155270
Marlen <zomadujsonov@gmail.com>
2024-10-09 20:55:54 +0600

    auth

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./SignIn.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import loginImage from "@/assets/images/signin_img.svg";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <header id={styles.Header}>
+        <div className="container">
+          <div className={styles.Header}>
+            <Image src={loginImage} alt="img" />
+            <Link href={"/auth/signin"}>Sign in</Link>
+          </div>
+        </div>
+        <hr />
+      </header>
+    </>
+  );
 };
 
 export default SignIn;
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,69 +1,117 @@
 "use client";
 import { useState } from "react";
 // @ts-ignore
-import InputMask from "react-input-mask";
 import styles from "./SignUp.module.scss";
+import { PhoneInput, defaultCountries } from "react-international-phone";
+import "react-international-phone/style.css";
+import registerImage from "@/assets/images/signin_img.svg";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+interface IInputComponentProps {
+  name?: string;
+  placeholder?: string;
+  value: string;
+  handleChange: (value: string) => void;
+  required?: boolean;
+}
 
 const SignIn = () => {
-  const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [accepted, setAccepted] = useState(false);
+  const [value, setValue] = useState("");
+  const route = useRouter();
 
-  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPhone(e.target.value);
+  const handlePhoneInputChange = (value: string) => {
+    setValue(value);
   };
 
+  const PhoneInputComponent = ({
+    name = "phone",
+    placeholder = "Phone number",
+    value,
+    handleChange,
+  }: IInputComponentProps) => (
+    <PhoneInput
+      defaultCountry="kg"
+      value={value}
+      onChange={handleChange}
+      className={"phone-input"}
+      placeholder={placeholder}
+    />
+  );
+
   return (
-    <div className={styles.container}>
-      <form className={styles.form}>
-        <h1>Registration</h1>
-        <label htmlFor="phone">Mobile phone</label>
+    <>
+      <header id={styles.Header}>
+        <div className="container">
+          <div className={styles.Header}>
+            <Image src={registerImage} alt="img" />
+            <Link href={"/auth/signin"}>Sign in</Link>
+          </div>
+        </div>
+        <hr />
+      </header>
+      <div className={styles.container}>
+        <form className={styles.form}>
+          <h1>Registration</h1>
+          {/* <label htmlFor="phone">Mobile phone</label>
         <InputMask
           mask="+996 999-999-999"
           value={phone}
           onChange={handlePhoneChange}
         >
           {() => <input id="phone" type="tel" className={styles.input} />}
-        </InputMask>
-        <small>This number will be used as your login</small>
+        </InputMask> */}
+          <small>This number will be used as your login</small>
 
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="email"
-          placeholder="Email"
-          className={styles.input}
-        />
-        <small>Password will be sent to this email</small>
-
-        <div className={styles.checkboxContainer}>
-          <input
-            type="checkbox"
-            id="accept"
-            checked={accepted}
-            onClick={() => setAccepted(!accepted)}
+          <PhoneInputComponent
+            value={value}
+            handleChange={handlePhoneInputChange}
           />
-          <label htmlFor="accept">
-            I accept the{" "}
-            <a href="/terms" target="_blank">
-              terms of the user agreement
-            </a>{" "}
-            and{" "}
-            <a href="/privacy" target="_blank">
-              agree to Privacy Policy
-            </a>
-          </label>
-        </div>
 
-        <button
-          type="submit"
-          disabled={!accepted}
-          className={styles.submitButton}
-        >
-          Sign up
-        </button>
-      </form>
-    </div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email"
+            className={styles.input}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <small>Password will be sent to this email</small>
+
+          <div className={styles.checkboxContainer}>
+            <input
+              type="checkbox"
+              id="accept"
+              checked={accepted}
+              onChange={() => setAccepted(!accepted)}
+            />
+            <label htmlFor="accept">
+              I accept the{" "}
+              <a href="/terms" target="_blank">
+                terms of the user agreement
+              </a>{" "}
+              and{" "}
+              <a href="/privacy" target="_blank">
+                agree to Privacy Policy
+              </a>
+            </label>
+          </div>
+
+          <Link href={"/service"}>
+            <button
+              type="submit"
+              disabled={!accepted}
+              className={styles.submitButton}
+            >
+              Sign up
+            </button>
+          </Link>
+        </form>
+      </div>
+    </>
   );
 };
 
